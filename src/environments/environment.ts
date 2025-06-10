248ac1898e13df772e1da2d38396a630d5727fab
Ezaryf <ezaryfhamdan@gmail.com>
2025-06-10 18:35:16 +0800

    100625 - Add enviroment for prod and dev

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiBase: 'https://intermediate-test-v-2-web-test.apps.ocp.tmrnd.com.my/api'
+};
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiBase: 'https://intermediate-test-v-2-web-test.apps.ocp.tmrnd.com.my/api'
+};
